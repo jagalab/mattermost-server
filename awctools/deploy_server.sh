c6dd36a935d98f7d7b85ec2a6bbf63ae8bdc71aa
adit <adityanuar@gmail.com>
2020-06-17 18:47:27 +0700

    add deploy server script

--- awctools/deploy_server.sh
+++ awctools/deploy_server.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+# create backup
+ssh mattermost@mm.jagalab.com 'sh /opt/mattermost/mm-backup.sh'
+# send and extract
 scp dist/mattermost-team-linux-amd64.tar.gz mattermost@mm.jagalab.com:/opt/mattermost
+ssh mattermost@mm.jagalab.com 'tar -xvf  /opt/mattermost/mattermost-team-linux-amd64.tar.gz --directory /opt/mattermost'
+# restore backup
+ssh mattermost@mm.jagalab.com 'sh /opt/mattermost/mm-restore.sh'
